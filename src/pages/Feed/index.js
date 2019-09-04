33df36d84d008796d1af996fcb2108a846d36b07
NYDino <rfapp00@gmail.com>
2019-09-04 14:14:10 -0300

    paginação finalizada

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -4,27 +4,38 @@ import {View, FlatList} from 'react-native';
 import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
 
 export default function App() {
-	const [Feed, setFeed] = useState([]);
+	const [feed, setFeed] = useState([]);
+	const [page, setPage] = useState(1);
+	const [total, setTotal] = useState(0);
+
+	async function loadPage(pageNumber = page) {
+		if (total && pageNumber > total) return;
+
+		console.log(pageNumber);
+
+		const response = await fetch(
+			`http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+		);
+
+		const data = await response.json();
+		const totalItems = response.headers.get('X-Total-Count');
+
+		setTotal(Math.floor(totalItems / 5));
+		setFeed([...feed, ...data]);
+		setPage(pageNumber + 1);
+	}
 
 	useEffect(() => {
-		async function loadFeed() {
-			const response = await fetch(
-				'http://localhost:3000/feed?_expand=author&_limit=5&_page=1'
-			);
-
-			const data = await response.json();
-
-			setFeed(data);
-		}
-
-		loadFeed();
+		loadPage();
 	}, []);
 
   return (
 		<View>
 			<FlatList 
-				data={Feed}
+				data={feed}
 				keyExtractor={post => String(post.id)}
+				onEndReached={() => loadPage()}
+				onEndReachedThreshold={0.1}
 				renderItem={({ item }) => (
 					<Post>
 						<Header>
